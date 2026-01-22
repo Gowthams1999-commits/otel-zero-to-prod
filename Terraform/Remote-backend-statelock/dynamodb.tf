61310524b4dc36e13bd502c6e7b485f9b391a6fb
Gowthams1999-commits <githubgowtham1999@gmail.com>
2026-01-22 11:55:01 +0530

    Add DynamoDB table for state locking
    Removed unused resources and added DynamoDB state lock configuration.

--- Terraform/Remote-backend-statelock/dynamodb.tf
+++ Terraform/Remote-backend-statelock/dynamodb.tf
@@ -1,64 +1,22 @@
-# random id generation for bucket suffix
-resource "random_id" "bucket-suffix" {
+# DynamoDB
 
-  byte_length = 8
+resource "aws_dynamodb_table" "state_lock" {
 
-}
+  name         = "vpc-state-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
+  attribute {
 
-
-locals {
-
-  common_tags = {
-
-
-    managed_by = "terraform"
-    project    = "otel"
+    name = "LockID"
+    type = "S"
 
   }
-}
-
-
-# Create s3 bucket
-resource "aws_s3_bucket" "remote_backend_bucket" {
-
-
-  bucket = "remote-backend-${random_id.bucket-suffix.hex}"
 
   lifecycle {
-
 
     prevent_destroy = true
 
   }
-}
 
-
-# Enable bucket versioning
-resource "aws_s3_bucket_versioning" "versioning_enabled" {
-
-
-  bucket = aws_s3_bucket.remote_backend_bucket.id
-  versioning_configuration {
-
-    status = "Enabled"
-
-  }
-
-}
-
-# Apply server side encryption for s3 bucket
-resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
-
-
-  bucket = aws_s3_bucket.remote_backend_bucket.id
-
-  rule {
-
-    apply_server_side_encryption_by_default {
-
-      sse_algorithm = "AES256"
-
-    }
-  }
 }
