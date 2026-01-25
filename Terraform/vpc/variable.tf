c36548de063eb56a74f2ec1ce5244e1cb12c6e9e
Gowthams1999-commits <githubgowtham1999@gmail.com>
2026-01-25 08:51:16 +0530

    Add VPC variables for CIDR and availability zones

---
+++ Terraform/vpc/variable.tf
@@ -0,0 +1,25 @@
+variable "vpc_cidr" {
+
+  type = string
+
+}
+
+variable "avz" {
+
+  type = list(string)
+
+}
+
+variable "public_subnet_cidr" {
+
+
+  type = list(string)
+
+}
+
+
+variable "private_subnet_cidr" {
+
+
+  type = list(string)
+
